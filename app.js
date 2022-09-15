202dec13c0f2143e740d1e8893131759c931bbfd
KyanD <dangtrungkien96@gmail.com>
2022-09-15 15:43:42 +0700

    fixed

--- app.js
+++ app.js
@@ -6,43 +6,37 @@ const cron = require("node-cron");
 const dotenv = require("dotenv").config();
 const fs = require("fs");
 const request = require("request");
-
-/*
-Basic mongo dump and restore commands, they contain more options you can have a look at man page for both of them.
-1. mongodump --db=rbac_tutorial --archive=./rbac.gzip --gzip
-2. mongorestore --uri="mongodb://maeda:uzMSUJR4pChmNvVtypSZsFWapmTfLqeR@dev.maeda.just.engineer:27017,dev.maeda.just.engineer:27018,dev.maeda.just.engineer:27019/dev_maedaexpress?replicaSet=rs0&authSource=admin&authMechanism=SCRAM-SHA-1" --archive=/root/scriptdb/public/dev.maedaexpress.com.gzip --gzip
-
-Using mongodump - without any args:
-  will dump each and every db into a folder called "dump" in the directory from where it was executed.
-Using mongorestore - without any args:
-  will try to restore every database from "dump" folder in current directory, if "dump" folder does not exist then it will simply fail.
-*/
-
-const date = new Date();
-
 // Database name
 const DB_NAME = process.env.DB_NAME;
 // File name backup
-const fileName = `${DB_NAME}-${date.getFullYear()}-${
-  date.getMonth() + 1
-}-${date.getDay()}T${date.getHours()}.${date.getMinutes()}.${date.getSeconds()}Z`;
-
-// Archive path
-const ARCHIVE_PATH = path.join(__dirname, "public", `${fileName}.gzip`);
-// Store oracle link
 const STORE_ORACLE = process.env.STORE_ORACLE;
-
+//Uri
+const DB_URI = process.env.MONGODB_URI;
 // 1. Cron expression for every 5 seconds - */5 * * * * *
 // 2. Cron expression for every night at 00:00 hours (0 0 * * * )
-// Note: 2nd expression only contains 5 fields, since seconds is not necessary
 
 // Scheduling the backup every 5 seconds (using node-cron)
-cron.schedule("0 0 */2 * * *", () => backupMongoDB());
+try {
+  cron.schedule("0 0 */2 * * *", () => {
+    const date = new Date();
+    // File name backup
+    const fileName = `${DB_NAME}-${date.getFullYear()}-${
+      date.getMonth() + 1
+    }-${date.getDate()}T${date.getHours()}.${date.getMinutes()}.${date.getSeconds()}Z`;
 
-function backupMongoDB() {
+    backupMongoDB(fileName);
+  });
+} catch (error) {
+  console.log(error);
+}
+
+function backupMongoDB(fileName) {
   try {
+    // Archive path
+    const ARCHIVE_PATH = path.join(__dirname, "public", `${fileName}.gzip`);
+
     const child = spawn("mongodump", [
-      `--uri=${process.env.DB_URI}`,
+      `--uri="${DB_URI}"`,
       `--archive=${ARCHIVE_PATH}`,
       "--gzip",
       "--forceTableScan",
@@ -53,19 +47,7 @@ function backupMongoDB() {
     });
 
     child.stderr.on("data", (data) => {
-      const fileContent = Buffer.from(data, "binary");
-      request(
-        {
-          url: `${STORE_ORACLE}${fileName}`,
-          method: "PUT",
-          headers: {
-            "cache-control": "no-cache",
-          },
-          encoding: null,
-          body: fs.createReadStream(ARCHIVE_PATH),
-        },
-        (error, response, body) => {}
-      );
+      console.log("stderr:\n", Buffer.from(data).toString());
     });
 
     child.on("error", (error) => {
@@ -76,11 +58,33 @@ function backupMongoDB() {
       if (code) console.log("Process exit with code:", code);
       else if (signal) console.log("Process killed with signal:", signal);
       else {
-        fs.unlinkSync(ARCHIVE_PATH);
-        console.log("Backup is successfully ✅");
+        const readStream = fs.createReadStream(ARCHIVE_PATH);
+        let chunks = [];
+        readStream.on("data", (chunk) => chunks.push(chunk));
+        readStream.on("end", () => {
+          const data = Buffer.concat(chunks);
+          const options = {
+            method: "PUT",
+            url: `${STORE_ORACLE}${fileName}.gzip`,
+            headers: {
+              "Content-Type": "application/octet-stream",
+              "Content-Encoding": "gzip",
+            },
+            encoding: null,
+            body: data,
+          };
+          var stats = fs.statSync(ARCHIVE_PATH);
+          request(options, function (error, response) {
+            if (error) throw new Error(error);
+            fs.unlinkSync(ARCHIVE_PATH);
+            console.log("Backup is successfully ✅");
+          });
+        });
       }
     });
+    return true;
   } catch (error) {
     console.log(error);
+    return false;
   }
 }
